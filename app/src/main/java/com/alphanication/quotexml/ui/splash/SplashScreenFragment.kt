d97433e2a502cedabcf1f2b64fca67c7534ddac5
vorobeye <russianfederation.vorobey@gmail.com>
2022-07-22 23:22:05 +0300

    add empty SplashScreenFragment.kt

---
+++ app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
@@ -0,0 +1,25 @@
+package com.alphanication.quotexml.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alphanication.quotexml.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+}
