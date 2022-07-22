d61adb633b8a8f71d8012c968bc4cd6fa6a447be
vorobeye <russianfederation.vorobey@gmail.com>
2022-07-22 23:19:56 +0300

    MainActivity navigation

--- app/src/main/java/com/alphanication/quotexml/ui/MainActivity.kt
+++ app/src/main/java/com/alphanication/quotexml/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.alphanication.quotexml.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.alphanication.quotexml.R
+import com.alphanication.quotexml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun setupNavigation() {
+
     }
 }
