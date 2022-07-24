e8af541f4cf60547a0200fd5f4da655d064e8b7d
vorobeye <russianfederation.vorobey@gmail.com>
2022-07-24 14:42:03 +0300

    SplashScreenFragment.kt viewbinding

--- app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
@@ -6,15 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.alphanication.quotexml.R
+import com.alphanication.quotexml.databinding.FragmentSplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : Fragment() {
 
+    private lateinit var binding: FragmentSplashScreenBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    ): View {
+        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
