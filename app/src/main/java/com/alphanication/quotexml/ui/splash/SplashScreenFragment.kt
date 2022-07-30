65e584828a6e9637542596300c0b787d34454e8e
vorobeye <russianfederation.vorobey@gmail.com>
2022-07-30 20:48:44 +0300

    correct SplashScreenFragment.kt, settings delay

--- app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
@@ -2,27 +2,34 @@ package com.alphanication.quotexml.ui.splash
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.alphanication.quotexml.databinding.FragmentSplashScreenBinding
+import androidx.navigation.fragment.findNavController
+import com.alphanication.quotexml.R
 import com.alphanication.quotexml.ui.base.BaseFragment
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : BaseFragment() {
 
-    private lateinit var binding: FragmentSplashScreenBinding
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    ): View? = inflater.inflate(R.layout.fragment_splash_screen, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        startMainQuoteScreen()
+    }
+
+    private fun startMainQuoteScreen() {
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_mainQuoteFragment)
+        }, SPLASH_SCREEN_DELAY_MS)
+    }
+
+    companion object {
+        private const val SPLASH_SCREEN_DELAY_MS = 800L
     }
 }
