3ed57c33508b510786a30976177dee828da237ab
vorobeye <russianfederation.vorobey@gmail.com>
2022-07-30 19:34:43 +0300

    fragments extends basefragment

--- app/src/main/java/com/alphanication/quotexml/ui/mainquote/MainQuoteFragment.kt
+++ app/src/main/java/com/alphanication/quotexml/ui/mainquote/MainQuoteFragment.kt
@@ -1,13 +1,13 @@
 package com.alphanication.quotexml.ui.mainquote
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.alphanication.quotexml.R
+import com.alphanication.quotexml.ui.base.BaseFragment
 
-class MainQuoteFragment : Fragment() {
+class MainQuoteFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/alphanication/quotexml/ui/splash/SplashScreenFragment.kt
@@ -5,27 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.alphanication.quotexml.R
 import com.alphanication.quotexml.databinding.FragmentSplashScreenBinding
-import com.alphanication.quotexml.di.MainCoroutineScope
-import com.alphanication.quotexml.utils.hide
-import com.alphanication.quotexml.utils.show
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import com.alphanication.quotexml.ui.base.BaseFragment
 
-@AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
-class SplashScreenFragment : Fragment() {
-
-    @Inject
-    @MainCoroutineScope
-    lateinit var mainCoroutineScope: CoroutineScope
+class SplashScreenFragment : BaseFragment() {
 
     private lateinit var binding: FragmentSplashScreenBinding
 
@@ -41,5 +25,4 @@ class SplashScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-
 }
