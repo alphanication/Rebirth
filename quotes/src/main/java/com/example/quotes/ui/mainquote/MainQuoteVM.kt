92827333347356d34966c0d237fb875b334491a8
vorobeye <russianfederation.vorobey@gmail.com>
2022-08-13 21:10:27 +0300

    add MainQuoteVM

---
+++ quotes/src/main/java/com/example/quotes/ui/mainquote/MainQuoteVM.kt
@@ -0,0 +1,9 @@
+package com.example.quotes.ui.mainquote
+
+import com.example.quotes.base.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainQuoteVM : BaseViewModel() {
+
+}
