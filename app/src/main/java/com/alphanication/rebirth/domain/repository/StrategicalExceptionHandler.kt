b6ccf7ec33cfc4ccf2cdf53330a916856f628e11
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-06 00:34:49 +0300

    add strategical error interceptor

--- app/src/main/java/com/alphanication/rebirth/domain/repository/StrategicalExceptionHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.alphanication.rebirth.domain.repository
-
-import com.alphanication.rebirth.ui.utils.StrategicalExceptions
-
-interface StrategicalExceptionHandler {
-
-    fun onError(exception: (StrategicalExceptions) -> Unit)
-}
--- app/src/main/java/com/alphanication/rebirth/ui/app/MainActivity.kt
+++ app/src/main/java/com/alphanication/rebirth/ui/app/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.alphanication.rebirth.ui.app
 
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alphanication.rebirth.R
-import com.alphanication.rebirth.domain.repository.StrategicalExceptionHandler
 import com.alphanication.rebirth.ui.utils.StrategicalExceptions
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        strategicalExceptionHandler()
+    }
+
+    private fun strategicalExceptionHandler() {
+        errorCallback = {
+            when (it) {
+                StrategicalExceptions.NETWORK_DISABLE -> showErrorNetwork()
+            }
+        }
+    }
+
+    private fun showErrorNetwork() {
+        Toast.makeText(applicationContext, resources.getString(R.string.network_error), Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private var errorCallback: ((StrategicalExceptions) -> Unit)? = null
+        private val uiPoster = Handler(Looper.getMainLooper())
+
+        fun onError(strategicalException: StrategicalExceptions) {
+            uiPoster.post {
+                errorCallback?.invoke(strategicalException)
+            }
+        }
     }
 }
