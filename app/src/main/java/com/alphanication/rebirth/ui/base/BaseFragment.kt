daa4267a3437f03a79c5f21a8c93013bbe041167
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-07 11:35:05 +0300

    correct exception handler show error base fragment

--- app/src/main/java/com/alphanication/rebirth/ui/base/BaseFragment.kt
+++ app/src/main/java/com/alphanication/rebirth/ui/base/BaseFragment.kt
@@ -2,9 +2,21 @@ package com.alphanication.rebirth.ui.base
 
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.alphanication.rebirth.domain.repository.NetworkMonitor
+import javax.inject.Inject
 
 abstract class BaseFragment : Fragment() {
 
-    fun showError(message: String) =
-        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
+    fun showError(message: String) {
+        /** Воробей Е. А., 07.01.2023
+         * Strategical exception handler выставлен на уровне interceptor'a retrofit */
+        if (networkMonitor.isConnected()) Toast.makeText(
+            requireContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 }
