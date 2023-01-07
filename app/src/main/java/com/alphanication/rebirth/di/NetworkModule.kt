7ac1ff1c39faf50cd14e07ebf672c6bcfd490df8
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-07 11:34:38 +0300

    add strings-en

--- app/src/main/java/com/alphanication/rebirth/di/NetworkModule.kt
+++ app/src/main/java/com/alphanication/rebirth/di/NetworkModule.kt
@@ -57,7 +57,7 @@ object NetworkModule {
 }
 
 class ErrorInterceptor(
-    private val networkMonitor: NetworkMonitor,
+    private val networkMonitor: NetworkMonitor
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
